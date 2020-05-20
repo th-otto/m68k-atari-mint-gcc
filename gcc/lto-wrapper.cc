9b9a6ced39ce4c44195bcd0333196b6e0309a9d5
Thorsten Otto <admin@tho-otto.de>
2020-05-20 09:58:30 +0200

    Accept /dev/null as alias for HOST_BIT_BUCKET on MinGW (needed to make selftests work)

--- gcc/lto-wrapper.cc
+++ gcc/lto-wrapper.cc
@@ -1712,7 +1712,8 @@ run_gcc (unsigned argc, char *argv[])
   if (!dumppfx)
     {
       if (!linker_output
-	  || strcmp (linker_output, HOST_BIT_BUCKET) == 0)
+	  || strcmp (linker_output, HOST_BIT_BUCKET) == 0
+	  || strcmp (linker_output, HOST_BIT_BUCKET2) == 0)
 	dumppfx = "a.";
       else
 	{
