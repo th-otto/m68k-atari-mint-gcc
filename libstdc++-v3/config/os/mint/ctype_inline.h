8323eb781a0623db345a08112779b191798a4432
Thorsten Otto <admin@tho-otto.de>
2023-08-19 13:22:18 +0200

    Get rid of some old ctype workarounds
    These are no longer needed with current mintlib

--- libstdc++-v3/config/os/mint/ctype_inline.h
+++ libstdc++-v3/config/os/mint/ctype_inline.h
@@ -1,6 +1,6 @@
 // Locale support -*- C++ -*-
 
-// Copyright (C) 2000-2017 Free Software Foundation, Inc.
+// Copyright (C) 2000-2023 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -41,96 +41,15 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   bool
   ctype<char>::
   is(mask __m, char __c) const
-  {
-    if (_M_table)
-      return _M_table[static_cast<unsigned char>(__c)] & __m;
-    else
-      {
-	bool __ret = false;
-	const size_t __bitmasksize = 15;
-	size_t __bitcur = 0; // Lowest bitmask in ctype_base == 0
-	for (; __bitcur <= __bitmasksize; ++__bitcur)
-	  {
-	    const mask __bit = static_cast<mask>(1 << __bitcur);
-	    if (__m & __bit)
-	      {
-		bool __testis;
-		switch (__bit)
-		  {
-		  case space:
-		    __testis = isspace(__c);
-		    break;
-		  case print:
-		    __testis = isprint(__c);
-		    break;
-		  case cntrl:
-		    __testis = iscntrl(__c);
-		    break;
-		  case upper:
-		    __testis = isupper(__c);
-		    break;
-		  case lower:
-		    __testis = islower(__c);
-		    break;
-		  case alpha:
-		    __testis = isalpha(__c);
-		    break;
-		  case digit:
-		    __testis = isdigit(__c);
-		    break;
-		  case punct:
-		    __testis = ispunct(__c);
-		    break;
-		  case xdigit:
-		    __testis = isxdigit(__c);
-		    break;
-		  case alnum:
-		    __testis = isalnum(__c);
-		    break;
-		  case graph:
-		    __testis = isgraph(__c);
-		    break;
-#ifdef _GLIBCXX_USE_C99_CTYPE_TR1
-		  case blank:
-		    __testis = isblank(__c);
-		    break;
-#endif
-		  default:
-		    __testis = false;
-		    break;
-		  }
-		__ret |= __testis;
-	      }
-	  }
-	return __ret;
-      }
-  }
+  { return (_M_table[static_cast<unsigned char>(__c) ] & __m); }
+
 
   const char*
   ctype<char>::
   is(const char* __low, const char* __high, mask* __vec) const
   {
-    if (_M_table)
-      while (__low < __high)
-	*__vec++ = _M_table[static_cast<unsigned char>(*__low++)];
-    else
-      {
-	// Highest bitmask in ctype_base == 11.
-	const size_t __bitmasksize = 15;
-	for (;__low < __high; ++__vec, ++__low)
-	  {
-	    mask __m = 0;
-	    // Lowest bitmask in ctype_base == 0
-	    size_t __i = 0;
-	    for (;__i <= __bitmasksize; ++__i)
-	      {
-		const mask __bit = static_cast<mask>(1 << __i);
-		if (this->is(__bit, *__low))
-		  __m |= __bit;
-	      }
-	    *__vec = __m;
-	  }
-      }
+    while (__low < __high)
+      *__vec++ = _M_table[static_cast<unsigned char>(*__low++)];
     return __high;
   }
 
@@ -138,13 +57,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   ctype<char>::
   scan_is(mask __m, const char* __low, const char* __high) const
   {
-    if (_M_table)
-      while (__low < __high
-	     && !(_M_table[static_cast<unsigned char>(*__low)] & __m))
-	++__low;
-    else
-      while (__low < __high && !this->is(__m, *__low))
-	++__low;
+    while (__low < __high && !this->is(__m, *__low))
+      ++__low;
     return __low;
   }
 
@@ -152,13 +66,8 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
   ctype<char>::
   scan_not(mask __m, const char* __low, const char* __high) const
   {
-    if (_M_table)
-      while (__low < __high
-	     && (_M_table[static_cast<unsigned char>(*__low)] & __m) != 0)
-	++__low;
-    else
-      while (__low < __high && this->is(__m, *__low) != 0)
-	++__low;
+    while (__low < __high && this->is(__m, *__low) != 0)
+      ++__low;
     return __low;
   }
 
